f1fd626e3fb3315800d3d775c45883fa9b6868b7
harshan89 <harshanweb@gmail.com>
2025-01-25 08:52:59 +0530

    exit scape chars

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="text-2xl">Hey I'm Harshan, going Live 28</h1>
+    <h1 className="text-2xl">Hey I&apos;m Harshan, going Live 28</h1>
   );
 }
