291a8ed83aef1d36432719c4a31c4f3832e65735
harshan89 <harshanweb@gmail.com>
2025-02-18 12:16:52 +0530

    Left panel pixel

--- app/components/Home/LeftPanel.tsx
+++ app/components/Home/LeftPanel.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const LeftPanel: FC<Props> = ({ leftMenu }) => {
   return (
-    <div className="flex gap-8 flex-col h-[500] w-[150] bg-yellow-300 pt-4 items-center">
+    <div className="flex gap-8 flex-col h-[500px] w-[150px] bg-yellow-300 pt-4 items-center">
       {leftMenu.map((menu, idx) => (
         <p key={idx}>{menu}</p>
       ))}
